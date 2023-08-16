efe8ffee4a143fb814c9e51ce5710239c37fc64d
Tboules <88787579+tmitboules@users.noreply.github.com>
2023-08-16 15:03:28 -0700

    add food

--- src/store/snakeStore.ts
+++ src/store/snakeStore.ts
@@ -5,6 +5,7 @@ type IDirection = "left" | "right" | "up" | "down";
 interface ISnake {
   x: number;
   y: number;
+  color?: string;
 }
 
 interface ISnakeState {
@@ -16,6 +17,7 @@ interface ISnakeState {
   direction: IDirection;
   changedDirDuringInterval: boolean;
   score: number;
+  speed: number;
 }
 
 interface ISnakeFunctions {
@@ -24,36 +26,33 @@ interface ISnakeFunctions {
   startGame: () => void;
   changeSnakeDirection: (e: KeyboardEvent) => void;
   moveSnake: () => void;
+  addTail: () => void;
   drawSnake: () => void;
   stopGame: () => void;
   collisionCheck: () => boolean;
   generateSnakeFood: () => void;
   clearSnakeFood: () => void;
   drawSnakeFood: () => void;
+  eatSnakeFood: () => void;
 }
 
 const CANVAS_HEIGHT = 1000;
 const CANVAS_WIDTH = 1000;
-const SNAKE_INTERVAL = 20;
-const SNAKE_BLOCK_SIZE = 19;
+const SNAKE_INTERVAL = 40;
+const SNAKE_BLOCK_SIZE = 39;
 
 const initialState: Omit<ISnakeState, "canvasCtx"> = {
   playing: false,
   snake: [
     { x: 0, y: 0 },
-    { x: 0, y: 20 },
     { x: 0, y: 40 },
-    { x: 0, y: 60 },
-    { x: 0, y: 80 },
-    { x: 0, y: 100 },
-    { x: 0, y: 120 },
-    { x: 0, y: 140 },
-    { x: 0, y: 160 },
   ],
   direction: "right",
   changedDirDuringInterval: false,
+
   food: null,
   score: 0,
+  speed: 500,
 };
 
 const useSnakeStore = create<ISnakeState & ISnakeFunctions>((set, get) => ({
@@ -74,11 +73,12 @@ const useSnakeStore = create<ISnakeState & ISnakeFunctions>((set, get) => ({
 
   startGame: () => {
     get().drawSnake();
+    get().generateSnakeFood();
 
     let intervalId = window.setInterval(function () {
       get().moveSnake();
       set({ changedDirDuringInterval: false });
-    }, 400);
+    }, get().speed);
 
     return set({ playing: true, intervalId });
   },
@@ -121,10 +121,24 @@ const useSnakeStore = create<ISnakeState & ISnakeFunctions>((set, get) => ({
     get().drawSnake();
   },
 
+  addTail: () => {
+    const snakeCopy = [...get().snake];
+    const snakeTail = snakeCopy[snakeCopy.length - 1];
+    const nodeBeforeTail = snakeCopy[snakeCopy.length - 2];
+
+    const xInterval = nodeBeforeTail.x - snakeTail.x;
+    const yInterval = nodeBeforeTail.y - snakeTail.y;
+
+    snakeCopy.push({ x: snakeTail.x - xInterval, y: snakeTail.y - yInterval });
+
+    set({ snake: snakeCopy });
+  },
+
   collisionCheck: () => {
     let collision = false;
     const snake = [...get().snake];
     const head = snake[0];
+    const food = get().food;
 
     const gameOver = () => {
       alert("game over");
@@ -148,6 +162,10 @@ const useSnakeStore = create<ISnakeState & ISnakeFunctions>((set, get) => ({
       }
     }
 
+    if (head.x == food?.x && head.y == food.y) {
+      get().eatSnakeFood();
+    }
+
     return collision;
   },
 
@@ -158,6 +176,7 @@ const useSnakeStore = create<ISnakeState & ISnakeFunctions>((set, get) => ({
     if (get().collisionCheck()) return;
 
     get().clearBoard();
+    get().drawSnakeFood();
 
     ctx.fillStyle = "green";
 
@@ -207,8 +226,10 @@ const useSnakeStore = create<ISnakeState & ISnakeFunctions>((set, get) => ({
       Math.floor((Math.random() * CANVAS_HEIGHT) / SNAKE_INTERVAL) *
       SNAKE_INTERVAL;
 
+    const foodColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
     get().clearSnakeFood();
-    set({ food: { x, y } });
+    set({ food: { x, y, color: foodColor } });
     get().drawSnakeFood();
   },
 
@@ -227,11 +248,22 @@ const useSnakeStore = create<ISnakeState & ISnakeFunctions>((set, get) => ({
     if (!ctx) return;
     const food = get().food;
 
-    ctx.fillStyle = "red";
+    ctx.fillStyle = food?.color ?? "red";
 
     if (food) {
       ctx.fillRect(food.x, food.y, SNAKE_BLOCK_SIZE, SNAKE_BLOCK_SIZE);
     }
+  },
+
+  eatSnakeFood: () => {
+    get().generateSnakeFood();
+    get().addTail();
+
+    set((state) => ({
+      removeTail: false,
+      score: state.score + 1,
+      speed: state.speed * 0.9,
+    }));
   },
 }));
 
